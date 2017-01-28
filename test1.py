11aa58da30bf3f8da89a31932f13c54ea7f2c0a5
hus.alsayed <hus.alsayed@gmail.com>
2017-01-28 12:22:14 -0500

    Test1 files

---
+++ test1.py
@@ -0,0 +1,14 @@
+
+num1 = 9
+num2 = 3
+
+v1 = num1 + num2
+v2 = num1 - num2
+v3 = num1 * num2
+v4 = num1 / num2
+
+print ("Sum value: ", v1)
+print ("Sub value: ", v2)
+print ("mul value: ", v3)
+print ("div value: ", v4)
+
